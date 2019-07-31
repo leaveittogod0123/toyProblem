42b002e041b725bc8e1776b07de594e642d113fa
yobs0814 <yobs0814@gmail.com>
2019-07-31 09:17:58 +0900

    toy-02 first try

--- 02-nonrepeatedCharacter/nonrepeatedCharacter.js
+++ 02-nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,17 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+    if(!string){
+        return;
+    }
+
+    let firstCharacter = string.charAt(0);
+    let nonRepeatedCharacter;
+    for (let i = 1; i < string.length; i++){
+        if(firstCharacter !== string.charAt(i)){
+            nonRepeatedCharacter = string.charAt(i);
+            break;
+        }
+    }
+    return nonRepeatedCharacter;
 };
