216258c58c0894d8e579d3d3faa16f650075858d
yobs0814 <yobs0814@gmail.com>
2019-08-02 09:15:25 +0900

    dp try

--- 04-nthFibonacci/nthFibonacci.js
+++ 04-nthFibonacci/nthFibonacci.js
@@ -22,6 +22,16 @@
 
 var nthFibonacci = function (n) {
   // TODO: implement me!
+
+    let memo = {};
+
+    memo[`${0}`] = 0;
+    memo[`${1}`] = 1;
+
+    for(let i = 2 ;  i<= n; i++){
+        memo[`${i}`] = memo[`${i-1}`] + memo[`${i-2}`];
+    }
+    return memo[`${n}`];
 };
 
 
