6109ee4b6f163c3e4a4d6a813dd03e91d2fb0af0
yobs0814 <yobs0814@gmail.com>
2019-08-08 10:54:36 +0900

    완탐으로  all pass

--- 08-largestProductOfThree/largestProductOfThree.js
+++ 08-largestProductOfThree/largestProductOfThree.js
@@ -16,13 +16,12 @@ var largestProductOfThree = function(array) {
     // 음수가 있어도 부호와 상관없이 크면 곱해줘야함.
 
     let sortedArr = array.sort((a,b) => Math.abs(b)-Math.abs(a));
-    console.log(sortedArr);
 
     // 완전탐색, 정렬, 그리디가 있을것 같은데
     // 정렬하고 그리디랑 전략이 비슷함.
     // 일단 완탐을 하면 3개를 뽑아야하니 O(N^3)
 
-    let max = Math.min(...sortedArr);
+    let max = sortedArr[0]*sortedArr[1]*sortedArr[2];
 
     for(let i = 0; i< sortedArr.length; i++){
         for(let j = i+1; j< sortedArr.length; j++){
@@ -35,5 +34,6 @@ var largestProductOfThree = function(array) {
     }
 
     return max;
+
 
 };
