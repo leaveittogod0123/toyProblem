a6cf5f900c4c57c1c9bc4af2497a4e32241f164d
yobs0814 <yobs0814@gmail.com>
2019-08-08 09:58:40 +0900

    toy-08 first try

--- 08-largestProductOfThree/largestProductOfThree.js
+++ 08-largestProductOfThree/largestProductOfThree.js
@@ -9,5 +9,44 @@
 
 
 var largestProductOfThree = function(array) {
-  // TODO: everything
+    // TODO: everything
+    // ans 만들고 0번값 저장
+    // 1번부터 ans =ans * 1번
+    // 반복
+    // reduce 활용법 공부
+
+    // 모두 음수일경우에는 가장 큰 작은 음수에 -1을 더해줘요. 맞는지 모르겠음.
+    // 문제 이해를 못하고 풀었음..
+    // 양수가 있을 경우에는 음수를 양수로 뒤집고 큰 세개의 수만 곱해서 리턴해줘요.
+
+    let EveryNegativeflag = array.every( (currentValue) => {
+        return currentValue < 0;
+    });
+
+    if(EveryNegativeflag){
+
+        let ans = array.sort()[array.length-1] -1;
+
+        return ans;
+
+    }
+
+    // 음수가 있을때는 양수로 바꿔줘요.
+
+    let negativeArray = [];
+
+
+    // 큰수가 앞으로 오게 내림차순해요.
+    array.sort( ( a,b ) => b-a );
+
+
+    return array.reduce((acc,cur,i) => {
+
+        if(i >= 3){
+            return acc;
+        }
+
+        return acc * cur;
+    });
+
 };
