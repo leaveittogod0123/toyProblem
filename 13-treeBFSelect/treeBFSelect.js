b7a341421782602779c618848ec12c85c7dac546
yobs0814 <yobs0814@gmail.com>
2019-08-16 12:38:31 +0900

    BFS로 풀음

--- 13-treeBFSelect/treeBFSelect.js
+++ 13-treeBFSelect/treeBFSelect.js
@@ -30,64 +30,51 @@
  * Basic tree that stores a value.
  */
 
-var Tree = function (value, depth = 0) {
+var Tree = function (value) {
     this.value = value;
-    this.depth = depth;
     this.children = [];
 };
 
 
 Tree.prototype.BFSelect = function (filter) {
     // return an array of values for which the function filter(value, depth) returns true
-    // let qu = [];
-    // let checked = {};
-    //
-    // qu.push(this);
-    // checked[`${this.value}`] = true;
-    //
-    // while (qu.length) {
-    //     let currentNode = qu.shift();
-    //
-    //     for (const child of currentNode.children) {
-    //         if (checked[`${child.value}`]) continue;
-    //
-    //         if (filter(child.value, child.depth)) {
-    //             checked[`${child.value}`] = true;
-    //             qu.push(child);
-    //         }
-    //
-    //     }
-    // }
-    //
-    // let ans = [];
-    // for (const node in checked) {
-    //     if (checked[`${node}`]) ans.push(node);
-    // }
-    //
-    // return ans;
+    let qu = [];
+    let checked = {};
     let ans = [];
+    let depth = 0;
+    let count = 0;
 
-    // ㅇㅓ디서 부터 순회를 시작할 건지? -> root.DFSSelct이니 this.value는 root이겠네
-    //
-    function traverse(currentNode, visited, depth) {
-
-        for (let child of currentNode.children) {
-            if (filter(child.value, depth)) {
-                visited.push(child.value);
-            }
-            traverse(child, visited, depth + 1);
-        }
-
-    }
-
-
-    if (filter(this.value, 0, 0)) {
+    qu.push(this);
+    checked[`${this.value}`] = true;
+    this.depth = depth;
+    if(filter(this.value, depth)){
         ans.push(this.value);
     }
 
-    traverse(this, ans, 1);
+
+    while (qu.length) {
+        let currentNode = qu.shift();
+        //currentNode의 자식수를 count에 받아온다.
+
+        for (const child of currentNode.children) {
+            child.depth = currentNode.depth + 1;
+            // if (checked[`${child.value}`]) continue; 일반적인 BFS로직에서는 원래는 넣어야 하는 코드,
+            // 그런데 주어진 데이터구조에서는
+            // 다른 노드에서 중복된 value 값을 가질 수 있다.
+            // BFS, DFS건 어쨋든 목적은 모든 노드를 돌아야한다. 그 목적에 맞지 않기 때문에 주석처리했다.
+            // 그런데 또 다른 조건이 무조건 한번씩 방문한다는 것이다. 그거 때문에 check 로직을 넣어줬다.
+            // 현재 데이터 구조에서는 한번 방문한 노드를 또 방문하지 않기 때문에 넣지 않았다.
+
+            if (filter(child.value, child.depth)) {
+                ans.push(child.value);
+            }
+            checked[`${child.value}`] = true;
+            qu.push(child);
+        }
+    }
 
     return ans;
+
 };
 
 /**
@@ -143,20 +130,3 @@ Tree.prototype.removeChild = function (child) {
         throw new Error("That node is not an immediate child of this tree");
     }
 };
-
-
-var root1 = new Tree(1);
-var branch2 = root1.addChild(2);
-var branch3 = root1.addChild(3);
-var leaf4 = branch2.addChild(4);
-var leaf5 = branch2.addChild(5);
-var leaf6 = branch3.addChild(6);
-var leaf7 = branch3.addChild(7);
-let ans = root1.BFSelect(function (value, depth) {
-    return value % 2;
-})
-
-console.log(ans);
-
-
-// [1, 3, 5, 7]
