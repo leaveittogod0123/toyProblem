3b046f1aafc1c73717b8fa4194205cd0a5998a85
yobs0814 <yobs0814@gmail.com>
2019-08-19 09:34:48 +0900

    15-binarySearch

--- 15-binarySearchArray/binarySearchArray.js
+++ 15-binarySearchArray/binarySearchArray.js
@@ -9,5 +9,23 @@
  */
 
 var binarySearch = function (array, target) {
+
+    let low = 0, high = array.length-1;
+
+
+    while(low <= high){
+
+        let mid = Math.floor((high + low)/2);
+        if( array[mid] === target){
+            return mid;
+        }else if( array[mid] > target){
+            high = mid-1;
+        }else{
+            low = mid+1;
+        }
+    }
+
+    return null;
 };
+
 
