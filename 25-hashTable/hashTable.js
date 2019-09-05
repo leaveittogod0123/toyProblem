d70b52a97dc80522df45d79ba7ede3560f72e3f0
yobs0814 <yobs0814@gmail.com>
2019-09-05 09:58:29 +0900

    toy-25

--- 25-hashTable/hashTable.js
+++ 25-hashTable/hashTable.js
@@ -3,37 +3,40 @@
  * The hashtable does not need to resize but it should still handle collisions.
  */
 
-var makeHashTable = function(){
-  var result = {};
-  var storage = [];
-  var storageLimit = 1000;
-  result.insert = function(/*...*/ 
-){
-    // TODO: implement `insert()`
-  };
+var makeHashTable = function () {
+    var result = {};
+    var storage = [];
+    var storageLimit = 1000;
+    result.insert = function (key,value) {
+        // TODO: implement `insert()`
+        const hash=getIndexBelowMaxForKey(key, storageLimit);
+        storage[hash] = value;
+    };
 
-  result.retrieve = function(/*...*/ 
-){
-    // TODO: implement `retrieve()`
-  };
+    result.retrieve = function (key) {
+        // TODO: implement `retrieve()`
+        const hash=getIndexBelowMaxForKey(key, storageLimit);
+        return storage[hash];
+    };
 
-  result.remove = function(/*...*/ 
-){
-    // TODO: implement `remove()`
-  };
+    result.remove = function (key) {
+        // TODO: implement `remove()`
+        const hash=getIndexBelowMaxForKey(key, storageLimit);
+        delete storage[hash];
+    };
 
-  return result;
+    return result;
 };
 
 // This is a "hashing function". You don't need to worry about it, just use it
 // to turn any string into an integer that is well-distributed between
 // 0 and max - 1
-var getIndexBelowMaxForKey = function(str, max){
-  var hash = 0;
-  for (var i = 0; i < str.length; i++) {
-    hash = (hash<<5) + hash + str.charCodeAt(i);
-    hash = hash & hash; // Convert to 32bit integer
-    hash = Math.abs(hash);
-  }
-  return hash % max;
+var getIndexBelowMaxForKey = function (str, max) {
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+        hash = (hash << 5) + hash + str.charCodeAt(i);
+        hash = hash & hash; // Convert to 32bit integer
+        hash = Math.abs(hash);
+    }
+    return hash % max;
 };
