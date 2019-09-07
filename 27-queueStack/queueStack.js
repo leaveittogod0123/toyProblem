eb88bade983274d61ac3b30b04013c8aee15308a
yobs0814 <yobs0814@gmail.com>
2019-09-07 11:05:08 +0900

    toy-27

--- 27-queueStack/queueStack.js
+++ 27-queueStack/queueStack.js
@@ -9,16 +9,25 @@
   */
 var Stack = function() {
 
+  this.data = [];
+
   // add an item to the top of the stack
-  this.push = function(){
+  this.push = function(input){
+    //this.data = [...this.data, input];
+    this.data.push(input);
   };
 
   // remove an item from the top of the stack
   this.pop = function(){
+    //
+    const output = this.data.pop();
+    // this.data = [...this.data];
+    return output;
   };
 
   // return the number of items in the stack
   this.size = function(){
+    return this.data.length;
   };
 };
 
@@ -30,18 +39,29 @@ var Queue = function() {
   var inbox = new Stack();
   var outbox = new Stack();
 
+  //inbox, outbox가 버퍼의 의미인가... 뭐지 모르겠네.
+
   // called to add an item to the `queue`
-  this.enqueue = function(){
+  this.enqueue = function(input){
     // TODO: implement `enqueue`
+    while(outbox.size()){
+      inbox.push(outbox.pop());
+    }
+    inbox.push(input);
   };
 
   // called to remove an item from the `queue`
   this.dequeue = function(){
     // TODO: implement `dequeue`
+    while(inbox.size()){
+      outbox.push(inbox.pop());
+    }
+    return outbox.pop();
   };
 
   // should return the number of items in the queue
   this.size = function(){
     // TODO: implement `size`
+    return inbox.size() || outbox.size();
   };
 };
