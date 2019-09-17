26120b8f1839bc95d084722e86e9040f1cee320f
yobs0814 <yobs0814@gmail.com>
2019-09-17 22:34:01 +0900

    Pass 21

--- 32-primeTester/primeTester.js
+++ 32-primeTester/primeTester.js
@@ -5,12 +5,43 @@
  */
 
 var primeTester = function(n) {
-  if(typeof n !== 'number' || n < 1 || n % 1 !== 0){
+  if(typeof n !== 'number' || n <= 1 || n % 1 !== 0){
     // n isn't a number or n is less than 1 or n is not an integer
     return false;
   }
+
   // TODO: return true if n is prime, false otherwise
-};
+
+  var upperLimit = Math.sqrt(Math.abs(n));
+  for (var i = 2; i <= upperLimit; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//소수 판별 === 에라토스테네스의 체
+var setPrime = function(n){
+  if(typeof n !== 'number' || n <= 1 || n % 1 !== 0){
+    // n isn't a number or n is less than 1 or n is not an integer
+    return [];
+  }
+
+  // TODO: return true if n is prime, false otherwise
+
+  let arr = new Array(n+2).fill();
+  let upperLimit = Math.sqrt(Math.abs(n));
+  arr[1] = true;
+  for (let i = 2; i <= upperLimit; i++) {
+    if(!arr[i]){
+      for(let j = i+i; j<=n; j+=i){
+        arr[j] = true;
+      }
+    }
+  }
+  return arr;
+}
 
 /* Extra credit: Write a function that generates a list of all prime numbers
  * in a user-specified range (inclusive). If you're not quite sure where to start,
@@ -19,6 +50,13 @@ var primeTester = function(n) {
  */
 
 var primeSieve = function (start, end) {
+  let primes = setPrime(end);
+  let ans = [];
+  for(let i=start; i<=end; i++){
+    if(!primes[i] && !ans.includes(i)){
+      ans.push(i);
+    }
+  }
+  return ans;
 };
-
 
